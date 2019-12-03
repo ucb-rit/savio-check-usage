c21d91c73279ac687b90b8da69f54ebdb845dfef
Viraat Chandra <viraat_chandra_24@berkeley.edu>
2019-12-03 12:25:17 -0800

    added templates for request codes

---
+++ check_usage.py
@@ -0,0 +1,7 @@
+start_time = ''  # unix time stamp
+end_time = ''  # unix time stamp
+account_name = ''  # str
+
+template = 'Usage for USER {} [{}, {}]: {} jobs, {} CPUHrs, {} SUs'
+
+# query jobs endpoint with account_name, start_time, end_time
