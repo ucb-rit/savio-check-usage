04de278e815afeff660ae4ac80a98ddcb06c87f5
Viraat Chandra <viraat_chandra_24@berkeley.edu>
2019-12-03 13:12:13 -0800

    added argument parsing

--- check_usage.py
+++ check_usage.py
@@ -1,7 +1,57 @@
-start_time = ''  # unix time stamp
-end_time = ''  # unix time stamp
-account_name = ''  # str
+import argparse
+import datetime
+
+
+docstr = '''
+This script shows user/project usage
+'''
+
+timestamp_format_complete = '%Y-%m-%dT%H:%M:%S'
+timestamp_format_minimal = '%Y-%m-%d'
+
+
+def valid_date(s):
+    '''check if date is in valid format(s)'''
+    complete, minimal = None, None
+
+    try:
+        complete = datetime.datetime.strptime(s, timestamp_format_complete)
+    except ValueError:
+        pass
+
+    try:
+        minimal = datetime.datetime.strptime(s, timestamp_format_minimal)
+    except ValueError:
+        pass
+
+    if not complete and not minimal:  # doesn't fit either format
+        raise argparse.ArgumentTypeError('Not a valid date: {}'.format(s))
+    else:
+        return complete or minimal
+
+
+parser = argparse.ArgumentParser(description=docstr)
+parser.add_argument('-A', '--account', required=True,
+                    help='check usage of this account')
+parser.add_argument('-s', '--start', type=valid_date,
+                    help='start time YYYY-MM-DD[THH:MM:SS]'
+                    '(DEFAULT: {}-06-01T00:00:00)'.format(datetime.datetime
+                                                          .now().year),
+                    default='{}-06-01T00:00:00'.format(datetime.datetime
+                                                       .now().year))
+parser.add_argument('-e', '--end', type=valid_date,
+                    help='end time YYYY-MM-DD[THH:MM:SS]'
+                    '(DEFAULT: Current TimeStamp)',
+                    default=datetime.datetime.now()
+                    .strftime(timestamp_format_complete))
+parser.parse_args()
+
+'''
+start_time = parser.start  # unix time stamp
+end_time = parser.end  # unix time stamp
+account_name = parser.account  # str
 
 template = 'Usage for USER {} [{}, {}]: {} jobs, {} CPUHrs, {} SUs'
 
 # query jobs endpoint with account_name, start_time, end_time
+'''
