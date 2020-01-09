3a1ce208eff27d0d1c5676f12636c4eea0309071
Viraat Chandra <viraat_chandra_24@berkeley.edu>
2020-01-09 21:42:02 +0530

    convert to python2

--- check_usage.py
+++ check_usage.py
@@ -1,6 +1,9 @@
 import argparse
 import datetime
-import requests
+
+# import requests
+import urllib2
+import urllib
 
 docstr = '''
 This script shows user/project usage
@@ -52,20 +55,30 @@ end = parsed.end
 output_template = 'Usage for USER {} [{}, {}]: '.format(account, start, end)
 output_template += '{} jobs, {} CPUHrs, {} SUs'
 
-# print('DEBUG: ', output_template)
-
 
 auth_token = '6905b2f4dc8798f5cff7c9af0fe93cab0dec193a'
 auth_header = {'Authorization': 'Token {}'.format(auth_token)}
 base_url = 'https://scgup-dev.lbl.gov:8443/mybrc-rest'
 
-url_account_usages = base_url + '/account_usages'
 account_usages_params = {
     'account': account,
     'start_time': start,
     'end_time': end
 }
 
-account_usages = requests.get(url=url_account_usages,
-                              params=account_usages_params,
-                              headers=auth_header)
+url_account_usages = base_url + '/account_usages' + '?' \
+    + urllib.urlencode(account_usages_params)
+
+req_account_usages = urllib2.Request(url_account_usages, auth_header)
+
+try:
+    res_account_usages = urllib2.urlopen(req_account_usages)
+    account_usages = res_account_usages.read()
+except urllib2.URLError:
+    raise SystemExit("ERROR: Failed to connect to backend...")
+
+# account_usages = requests.get(url=url_account_usages,
+#                               params=account_usages_params,
+#                               headers=auth_header)
+
+print('DEBUG: ', account_usages)
