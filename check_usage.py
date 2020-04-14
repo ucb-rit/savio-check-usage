88e849422a049e28c1894f951ba512eacb74afb4
Viraat Chandra <viraat.chandra@hotmail.com>
2020-04-14 22:47:38 +0530

    make script identical to check_usage.sh on login nodes. moved to production instance of rest api.

--- check_usage.py
+++ check_usage.py
@@ -2,6 +2,7 @@
 import argparse
 import datetime
 import time
+import getpass
 
 import urllib2
 import urllib
@@ -10,13 +11,14 @@ import json
 
 VERSION = 0.1
 docstr = '''
-[version: {}] This script shows user/project usage
+[version: {}]
 '''.format(VERSION)
 
 ##### params #####
 
-BASE_URL = 'https://scgup-dev.lbl.gov:8443/mybrc-rest'
-# BASE_URL = 'http://localhost:8880/mybrc-rest'
+# BASE_URL = 'http://mybrc.brc.berkeley.edu/mybrc-rest/'
+# BASE_URL = 'https://scgup-dev.lbl.gov:8443/mybrc-rest'
+BASE_URL = 'http://localhost:8880/mybrc-rest'
 
 timestamp_format_complete = '%Y-%m-%dT%H:%M:%S'
 timestamp_format_minimal = '%Y-%m-%d'
@@ -37,9 +39,11 @@ def valid_date(s):
         pass
 
     if not complete and not minimal:  # doesn't fit either format
-        raise argparse.ArgumentTypeError('Not a valid date: {}'.format(s))
+        raise argparse.ArgumentTypeError(
+            'Invalid time specification {}'.format(s))
     else:
         return s
+
 
 def process_date_time(date_time):
     try:
@@ -48,27 +52,24 @@ def process_date_time(date_time):
         return time.mktime(time.strptime(date_time, timestamp_format_minimal))
 
 
-
 ##### parse arguments #####
 
 
-
+current_month = datetime.datetime.now().month
+current_year = datetime.datetime.now().year
+default_start = current_year if current_month >= 6 else (current_year - 1)
 parser = argparse.ArgumentParser(description=docstr)
 
-parser.add_argument('-U', '--user',
-                                help='check usage of this user')
-parser.add_argument('-A', '--account',
-                                help='check usage of this account')
+parser.add_argument('-u', dest='user',
+                    help='check usage of this user')
+parser.add_argument('-a', dest='account',
+                    help='check usage of this account')
 
-parser.add_argument('-s', '--start', type=valid_date,
-                    help='start time YYYY-MM-DD[THH:MM:SS]'
-                    '(DEFAULT: {}-06-01T00:00:00)'.format(datetime.datetime
-                                                          .now().year),
-                    default='{}-06-01T00:00:00'.format(datetime.datetime
-                                                       .now().year))
-parser.add_argument('-e', '--end', type=valid_date,
-                    help='end time YYYY-MM-DD[THH:MM:SS]'
-                    '(DEFAULT: Current TimeStamp)',
+parser.add_argument('-s', dest='start', type=valid_date,
+                    help='starttime for the query period (YYYY-MM-DD[THH:MM:SS])',
+                    default='{}-06-01T00:00:00'.format(default_start))
+parser.add_argument('-e', dest='end', type=valid_date,
+                    help='endtime for the query period (YYYY-MM-DD[THH:MM:SS])',
                     default=datetime.datetime.now()
                     .strftime(timestamp_format_complete))
 parsed = parser.parse_args()
@@ -79,13 +80,11 @@ _end = parsed.end
 start = process_date_time(_start)
 end = process_date_time(_end)
 
-if not (user or account):
-    parser.error('Atleast one of -U/--user and -A/--account required.')
-
+if not user and not account:
+    user = getpass.getuser()
 
 
 ##### prepare params #####
-
 
 
 request_urls = {}
@@ -105,7 +104,8 @@ if user:
     output_headers['user'] = output_header
 
 if account:
-    output_header = 'Usage for ACCOUNT {} [{}, {}]: '.format(account, _start, _end)
+    output_header = 'Usage for ACCOUNT {} [{}, {}]:'.format(
+        account, _start, _end)
     request_params = {
         'start_time': start,
         'end_time': end,
@@ -117,24 +117,8 @@ if account:
     request_urls['account'] = url_usages
     output_headers['account'] = output_header
 
-if user and account:
-    output_header = 'Usage for USER:ACCOUNT {}:{} [{}, {}]: '.format(user, account, _start ,_end)
-    request_params = {
-        'start_time': start,
-        'end_time': end,
-        'user': user,
-        'account': account
-    }
-    url_usages = BASE_URL + '/user_account_usages?' + \
-        urllib.urlencode(request_params)
-
-    request_urls['user_account'] = url_usages
-    output_headers['user_account'] = output_header
-
-
 
 ##### output functions #####
-
 
 
 def get_allocation_for_account(account):
@@ -151,24 +135,7 @@ def get_allocation_for_account(account):
         response = []
 
     if len(response) != 0:
-        return response[0]['allocation'] # best match
-
-
-def process_user_account_usages(req_url):
-    req = urllib2.Request(req_url)
-    response = json.loads(urllib2.urlopen(req).read())
-    response = response['results']
-
-    if len(response) == 0:
-        print 'No such projects (', account, ') of user:', user
-        return
-
-    print output_headers['user_account']
-    for single in response:
-        user_account = single['user_account']
-        usage = single['usage']
-
-        print '\taccount', user_account['account'], 'usage by', user, 'is', usage, 'out of user allocation of', user_account['allocation']
+        return response[0]['allocation']  # best match
 
 
 def process_account_usages(req_url):
@@ -177,18 +144,14 @@ def process_account_usages(req_url):
     response = response['results']
 
     if len(response) == 0:
-        print 'No such user have tracked usages:', account
+        print 'ERROR: Account', account, 'not defined.'
         return
 
-    print output_headers['account']
-    for single in response:
-        usage = single['usage']
-        account_project = single['account']
-        account_allocation = get_allocation_for_account(account)
-
-        print '\taccount',  account_project, 'total usage is', usage, 'out of project allocation of', account_allocation
-
-    print
+    single = response[0]
+    usage = single['usage']
+    account_project = single['account']
+    account_allocation = get_allocation_for_account(account)
+    print output_headers['account'], usage, 'SUs used from an allocation of', account_allocation, 'SUs.'
 
 
 def process_user_usages():
@@ -197,22 +160,20 @@ def process_user_usages():
     response = response['results']
 
     if len(response) == 0:
-        print 'No such accounts have tracked usages:', user
+        print 'ERROR: User', user, 'not defined.'
         return
 
-    print output_headers['user']
+    usage = 0.0
     for single in response:
-        usage = single['usage']
-        user_account = single['user_account']
+        try:
+            usage += float(single['usage'])
+        except ValueError:
+            pass
 
-        print '\taccount', user_account['account'], 'usage by', user, 'is', usage, 'out of user allocation of', user_account['allocation']
-
-    print
-
+    print output_headers['user'], usage, 'SUs used.'
 
 
 ##### get data #####
-
 
 
 for req_type, req_url in request_urls.items():
@@ -220,16 +181,12 @@ for req_type, req_url in request_urls.items():
         if req_type == 'user':
             process_user_usages()
 
-        if req_type == 'user_account':
-            process_user_account_usages(req_url)
-
         if req_type == 'account':
             process_account_usages(req_url)
 
     except urllib2.URLError, e:
-        print (e.reason)
-        pass # url error
+        print(e.reason)
+        pass  # url error
 
     except ValueError, e:
-        pass # json decode error
-
+        pass  # json decode error
